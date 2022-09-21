5a5c95ee06045c17c8f4e619fa2d3dd1a614ddf9
edilanesilva <edilanesantos75@gmail.com>
2022-09-21 13:44:22 -0300

    modified lamp

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Mini projetos vanilla JavaScript iniciantes
+
+## "Não existe outra maneira de aprender programação, a não colocando a mão na massa"
--- lampada/main.js
+++ lampada/main.js
@@ -2,27 +2,13 @@ const turnOn = document.querySelector("#turnOn");
 const turnOff = document.querySelector("#turnOff");
 const lamp = document.querySelector("#lamp");
 
-function isLampBroken() {
-  return lamp.src.indexOf("quebrada") > -1;
+function ligarLampada() {
+  lamp.src = "assets/img/ligada.png";
 }
 
-function lampOn() {
-  if (!isLampBroken()) {
-    lamp.src = "assets/img/ligada.png";
-  }
-}
-function lampOff() {
-  if (!isLampBroken()) {
-    lamp.src = "assets/img/desligada.png";
-  }
+function desligarLampada() {
+  lamp.src = "assets/img/desligada.png";
 }
 
-function lampBroken() {
-  lamp.src = "assets/img/quebrada.png";
-}
-
-turnOn.addEventListener("click", lampOn);
-turnOff.addEventListener("click", lampOff);
-lamp.addEventListener("mouseover", lampOn);
-lamp.addEventListener("mouseleave", lampOff);
-lamp.addEventListener("dblclick", lampBroken);
+turnOn.addEventListener("click", ligarLampada);
+turnOff.addEventListener("click", desligarLampada);
